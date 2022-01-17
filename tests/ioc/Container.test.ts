40439b3817a6fea539a2cdaeaeb3acbd2b1859a2
ivorobioff <igor.vorobioff@gmail.com>
2022-01-17 20:33:12 +0200

    some updates

--- tests/ioc/Container.test.ts
+++ tests/ioc/Container.test.ts
@@ -4,20 +4,35 @@ test('registers an instance, and then checks and retrieves it', () => {
     let container  = new Container();
     let instance = new SomeService();
 
+    expect(container.has(SomeService)).toBeFalsy()
+
     container.registerInstance(SomeService, instance);
 
     expect(container.has(SomeService)).toBeTruthy();
     expect(container.get(SomeService)).toEqual(instance);
 });
 
-test('registers a factory, then checks and retrieves the instance', () => {
+test('registers a factory, and then checks and retrieves the instance', () => {
     let container  = new Container();
     let instance = new SomeService();
+
+    expect(container.has(SomeService)).toBeFalsy()
 
     container.registerFactory(SomeService, () => instance);
 
     expect(container.has(SomeService)).toBeTruthy();
     expect(container.get(SomeService)).toEqual(instance);
+});
+
+test('registers a type, and then checks and retrieves its instance', () => {
+    let container  = new Container();
+    
+    expect(container.has(SomeService)).toBeFalsy()
+
+    container.registerType(SomeService);
+
+    expect(container.has(SomeService)).toBeTruthy();
+    expect(container.get(SomeService)).toBeInstanceOf(SomeService);
 });
 
 class SomeService {
