46ae22a97b7b9062e4a8a1a0526f8d77a71c6e6c
ivorobioff <igor.vorobioff@gmail.com>
2022-01-17 22:02:20 +0200

    some updates

--- tests/ioc/Container.test.ts
+++ tests/ioc/Container.test.ts
@@ -1,7 +1,7 @@
 import Container from "./../../src/ioc/Container";
 
 test('registers an instance, and then checks and retrieves it', () => {
-    let container  = new Container();
+    let container = new Container();
     let instance1 = new SomeService();
     let instance2 = new SomeService();
 
@@ -19,7 +19,7 @@ test('registers an instance, and then checks and retrieves it', () => {
 });
 
 test('registers a factory, and then checks and retrieves the instance', () => {
-    let container  = new Container();
+    let container = new Container();
     let instance1 = new SomeService();
     let instance2 = new SomeService();
 
@@ -37,8 +37,8 @@ test('registers a factory, and then checks and retrieves the instance', () => {
 });
 
 test('registers a type, and then checks and retrieves its instance', () => {
-    let container  = new Container();
-    
+    let container = new Container();
+
     expect(container.has(SomeService)).toBeFalsy();
     expect(container.has('some')).toBeFalsy();
 
@@ -57,17 +57,44 @@ test('registers a type, and then checks and retrieves its instance', () => {
 });
 
 test('register and then override', () => {
-    let container  = new Container();
+    let container = new Container();
 
-    var instance1  = new SomeService();
-    var instance2  = new SomeService();
+    var instance1 = new SomeService();
+    var instance2 = new SomeService();
 
     container.registerType(SomeService);
     container.registerInstance(SomeService, instance1);
     container.registerFactory(SomeService, () => instance2);
 
     expect(container.get(SomeService)).toBe(instance2);
+});
 
+
+test('register and retrieve and register again', () => {
+    let container = new Container();
+
+    container.registerType(SomeService);
+
+    container.get(SomeService);
+
+    expect(() => container.registerType(SomeService))
+        .toThrowError(Error('Container is locked - nothing can be registered!'));
+});
+
+test('register factoring producing nothing', () => {
+    let container = new Container();
+
+    container.registerFactory(SomeService, () => null);
+
+    expect(() => container.get(SomeService))
+        .toThrowError(Error("'SomeService' not created!"));
+});
+
+test('missing', () => {
+    let container = new Container();
+
+    expect(() => container.get(SomeService))
+        .toThrowError(Error("'SomeService' not registered!"));
 });
 
 class SomeService {
